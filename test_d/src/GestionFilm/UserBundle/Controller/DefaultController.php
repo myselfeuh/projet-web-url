ba19dbe12fc249711babfc1a377f77b7a232f285
Guillaume Penaud <guillaume.penaud@gmail.com>
2014-04-03 09:48:49 +0200

    Création du bindle user pour le TD de Gestion des films

---
+++ test_d/src/GestionFilm/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GestionFilm\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('GestionFilmUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ test_d/src/GestionFilm/UserBundle/GestionFilmUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GestionFilm\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GestionFilmUserBundle extends Bundle
+{
+}
