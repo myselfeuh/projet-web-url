0ccb9a9b14f2d1d08dcb1143d9f1ff3af79fbfdd
Guillaume Penaud <guillaume.penaud@gmail.com>
2014-04-29 20:43:56 +0200

    Mise à jour de la partie Stat

--- src/UrlReducer/CoreBundle/Controller/StatController.php
+++ src/UrlReducer/CoreBundle/Controller/StatController.php
@@ -10,22 +10,61 @@ class StatController extends Controller {
 	/**
 	 *
 	 */
-	public function frequencyAction() {
-		$aRenderingData = array();
-
-		$chart = new BarChart();
-
-        $chart  ->setWidth(200)
-                ->setHeight(200)
-                ->setDatas(array(200, 100, 50));
-
-        $url = $this->get('leg_google_charts')->build($chart);
-
-        $aRenderingData['chart_url'] = $url;
-
+	public function frequencyAction() { 
 		return $this->render(
 		    'UrlReducerCoreBundle:Stat:stat.layout.html.twig', 
-		    $aRenderingData
+		    array(
+		    	'chart_urls' => array(
+    				"Taux d'usage" 				=> $this->getFrequencyLineChart(),
+    				"Répartition par heure" 	=> $this->getFrequencyByHourPieChart(),
+    				"Répartition par semaine" 	=> $this->getFrequencyByWeekPieChart() 
+    			) 
+    		)
 		);
+	}
+
+	/**
+	 *
+	 */
+	public function getFrequencyLineChart() {
+		$aDatas = array(200, 100, 50);
+
+		$oChart = new BarChart();
+
+        $oChart->setWidth(300)
+               ->setHeight(300)
+               ->setDatas($aDatas);
+
+        return $this->get('leg_google_charts')->build($oChart);
+	}
+
+	/**
+	 *
+	 */
+	public function getFrequencyByHourPieChart() {
+		$aDatas = array(200, 100, 50);
+
+		$oChart = new BarChart();
+
+        $oChart->setWidth(300)
+               ->setHeight(300)
+               ->setDatas($aDatas);
+
+        return $this->get('leg_google_charts')->build($oChart);
+	}
+
+	/**
+	 *
+	 */
+	public function getFrequencyByWeekPieChart() {
+		$aDatas = array(200, 100, 50);
+
+		$oChart = new BarChart();
+
+        $oChart->setWidth(300)
+               ->setHeight(300)
+               ->setDatas($aDatas);
+
+        return $this->get('leg_google_charts')->build($oChart);	
 	}
 }
